d5ef2ef84620c11f0bbd4c0796830de70c16f512
Michael <michael@michaelngssimac>
2018-02-27 20:35:28 +0100

    Added Pius logo to navigation controller of home screen.

--- Pius-App/ViewController.swift
+++ Pius-App/ViewController.swift
@@ -16,12 +16,32 @@ class ViewController: UIViewController {
 
     var menuIsOpen : Bool = false;
     
+    // Add Pius logo as navigation bar title on initial view.
+    func addNavBarLogo() {
+        let navigationController = self.navigationController!;
+        let image = #imageLiteral(resourceName: "pius-logo-transparent");
+        let imageView = UIImageView(image: image);
+        
+        let bannerWidth = navigationController.navigationBar.frame.size.width;
+        let bannerHeight = navigationController.navigationBar.frame.size.height;
+        
+        let bannerX = bannerWidth / 2 - image.size.width / 2;
+        let bannerY = bannerHeight / 2 - image.size.height / 2;
+        
+        imageView.frame = CGRect(x: bannerX, y: bannerY, width: image.size.width, height: image.size.height);
+        imageView.contentMode = .scaleAspectFit;
+
+        navigationItem.titleView = imageView;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set color of navigation bar.
         self.navigationController?.navigationBar.barTintColor = UIColor(red:0.337, green:0.631, blue:0.824, alpha:1.0);
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white];
+        
+        self.addNavBarLogo();
         
         // Set menu shadow.
         self.menuView.layer.shadowOpacity = 1;
