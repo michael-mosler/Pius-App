2a85b88e6912cbec21b7ec8e9e46868ce36e26c3
Michael Mosler-Krings <rmkrings@web.de>
2018-03-24 19:49:25 +0100

    Safepoint before adding view which contains WebKitView. Intention is direct tab gesture to this view when sidebar menu is shown.

--- Pius-App/ViewController.swift
+++ Pius-App/ViewController.swift
@@ -9,15 +9,34 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet var tabGestureRecognizer: UITapGestureRecognizer!
     @IBOutlet weak var menuLeadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var menuView: UIView!
     @IBOutlet weak var webView: WKWebView!
     
     // Indicates if sidebar menu is open or closed.
     var menuIsOpen : Bool = false;
-    
+
+    @IBAction func tabAction(_ sender: Any) {
+        if (menuIsOpen) {
+            menuLeadingConstraint.constant = -180;
+            menuIsOpen = false;
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            });
+        }
+    }
+        
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return true;
+    }
+
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true;
+    }
+
     // Add Pius logo as navigation bar title on initial view.
     func addNavBarLogo() {
         let navigationController = self.navigationController!;
@@ -54,14 +73,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Set color of navigation bar.
-        self.navigationController?.navigationBar.barTintColor = UIColor(red:0.337, green:0.631, blue:0.824, alpha:1.0);
-        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white];
+        navigationController?.navigationBar.barTintColor = UIColor(red:0.337, green:0.631, blue:0.824, alpha:1.0);
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white];
         
-        self.addNavBarLogo();
+        addNavBarLogo();
         
         // Set menu shadow.
-        self.menuView.layer.shadowOpacity = 1;
-        self.menuView.layer.shadowRadius = 6;
+        menuView.layer.shadowOpacity = 1;
+        menuView.layer.shadowRadius = 6;
         
         // Enable refresh in WebKit View.
         let refreshControl = UIRefreshControl();
@@ -69,7 +88,12 @@ class ViewController: UIViewController {
         webView.scrollView.addSubview(refreshControl);
         
         // Load web view on initial view.
-        self.loadWebView();
+        loadWebView();
+        
+        tabGestureRecognizer.numberOfTapsRequired = 1;
+        tabGestureRecognizer.delegate = self;
+        
+        webView.addGestureRecognizer(tabGestureRecognizer);
     }
 
     override func didReceiveMemoryWarning() {
@@ -83,7 +107,7 @@ class ViewController: UIViewController {
         } else {
             self.menuLeadingConstraint.constant = 0;
         }
-        
+
         UIView.animate(withDuration: 0.3, animations: {
             self.view.layoutIfNeeded()
         })
