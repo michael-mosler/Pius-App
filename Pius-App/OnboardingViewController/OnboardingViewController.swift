03b7ddad8bdb1c2ed59091a9b6f6e16e6f8a3d73
Michael Mosler-Krings <rmkrings@gmail.com>
2019-02-23 12:27:02 +0100

    Updated Welcome Screen for 2.2.

--- Pius-App/OnboardingViewController/OnboardingViewController.swift
+++ Pius-App/OnboardingViewController/OnboardingViewController.swift
@@ -20,13 +20,13 @@ class OnboardingViewController: UITableViewController {
      */
     
     @IBOutlet weak var startAppButton: UIButton!
-    private let rowHeights: [CGFloat] = [110, 44, 100, 44, 100, 145, 100];
+    private let rowHeights: [CGFloat] = [110, 44, 100, 100, 44, 100, 100, 145, 100];
     private let offset: CGFloat = 32;
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard indexPath.row >= 7 else { return rowHeights[indexPath.row]; };
+        guard indexPath.row >= rowHeights.count else { return rowHeights[indexPath.row]; };
 
-        if indexPath.row == 7 {
+        if indexPath.row == rowHeights.count {
             let totalHeight = rowHeights.reduce(0, { x, y in x + y});
             return max(tableView.frame.height - totalHeight - startAppButton.frame.height - offset, startAppButton.frame.height + 12);
         } else {
